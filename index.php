ee00d3362f2cfca9b78fd140ce1aafcc8a97d0b3
Maylon Henrique <48722408+MaylonHenrique@users.noreply.github.com>
2019-04-23 11:01:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+  include("cabecalho.php");
+  ?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Sobre mim</h2>
+      <h5>Minha Foto:</h5>
+      <div class="fakeimg"><img src="anythinggoes-600x400.jpg" width="170px" height="200px"/></div>
+      <p>QualQuer Coisa Serve</p>
+      <h3>Alguns Links</h3>
+      <p>Oque Faço:</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Trabalho</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Estudo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Durmo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Me Alimento</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Fico Feliz</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Aula de Responsividade</h2>
+      <h5>Framework Bootstrap 41, 16 de abril de 2019</h5>
+      <div class="fakeimg"><img src="tela.jpg" alt="" width="300px" height="200px"> </div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<?php
+   include("rodape.php");
+   ?>
---
+++ rodape.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Doutor Maylon Henrique</p>
+  <p>Curso Técnico em informatica - Integrado</p>
+  <p>Turma: 3° Informatica</p>
+</div>
+
+</body>
+</html>
